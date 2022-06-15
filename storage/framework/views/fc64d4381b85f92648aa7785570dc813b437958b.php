d55b1ce35f9b4da1bb40aa1a42cd746483783299
Fenrz <96644164+Fenrz@users.noreply.github.com>
2022-06-15 23:19:23 +0700

    Add files via upload

---
+++ storage/framework/views/fc64d4381b85f92648aa7785570dc813b437958b.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title><?php echo $__env->yieldContent('title', $title); ?></title>
+</head>
+
+<body>
+    <div class="container">
+        <h1><?php echo $__env->yieldContent('title', $title); ?></h1>
+        <?php echo $__env->yieldContent('content'); ?>
+
+    </div>
+</body>
+</html><?php /**PATH C:\xampp\htdocs\laravel-tugasWR\resources\views/app.blade.php ENDPATH**/ ?>
